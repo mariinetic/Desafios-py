9e98bc5684cb42072d7aaf190eca1565a2eb4631
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-11-25 19:07:32 -0300

    add exercises

---
+++ gato.py
@@ -0,0 +1,6 @@
+def gato_mia():
+    print(" /\_/\  ")
+    print("( o.o ) Meow~")
+    print(" > ^ <  ")
+
+gato_mia()
--- heart.py
+++ heart.py
@@ -1,34 +1,8 @@
-import curses
+import time
 
-def draw_heart(stdscr):
-    heart = [
-        "  **     **  ",
-        " *  *   *  * ",
-        "*    * *    *",
-        "*     *     *",
-        " *         * ",
-        "  *       *  ",
-        "   *     *   ",
-        "    *   *    ",
-        "     * *     ",
-        "      *      "
-    ]
-
-    height, width = stdscr.getmaxyx()
-    y = height // 2 - len(heart) // 2
-    x = width // 2 - len(heart[0]) // 2
-
-    while True:
-        stdscr.clear()
-        
-        for i, line in enumerate(heart):
-            if y + i < height and x < width:
-                stdscr.addstr(y + i, x, line)
-        
-        stdscr.refresh()
-
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
-
-curses.wrapper(draw_heart)
+for _ in range(3):
+    print("  ❤  ❤  ")
+    print("❤      ❤")
+    print("  ❤  ❤  ")
+    time.sleep(0.5)
+    print("\n")
